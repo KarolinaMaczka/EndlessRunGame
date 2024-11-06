94ab031359f21225f1ea2ffaf3607cbe81995845
Łukasz Lepianka <76851989+Luki308@users.noreply.github.com>
2024-11-06 23:31:14 +0100

    final integration

--- difficulty/difficulty_logic.py
+++ difficulty/difficulty_logic.py
@@ -1,8 +1,8 @@
 from camera_reading.read_camera import EmotionHolder
 
 class DifficultyLogic():
-    def __init__(self):
-        self.emotion_holder = EmotionHolder()
+    def __init__(self, emotion_holder: EmotionHolder):
+        self.emotion_holder = emotion_holder
         self.difficulty_level = 1
         self.difficulty_level_max = 2
         self.difficulty_level_min = 1
