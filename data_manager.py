1ccc567385d72fa91fd2503cc6840d81bf157453
Łukasz Lepianka <76851989+Luki308@users.noreply.github.com>
2024-11-09 20:23:29 +0100

    refactor: update DataManager to use multiprocessing Manager for player emotions
    (cherry picked from commit 9389294e66eb3495fb1db4b9e3bb70382558b4ca)

--- data_manager.py
+++ data_manager.py
@@ -4,13 +4,14 @@ import json
 
 from config.config import config
 
+from multiprocessing import Manager
 
 class DataManager:
-    def __init__(self):
+    def __init__(self, list_manager: Manager): # type: ignore
         self.obstacle_data = []
         self.hit_obstacles = []
         self.player_satisfaction = -1
-        self.player_emotions = []
+        self.player_emotions = list_manager.list()
         self.__folder = config['player_data']['player_data.folder']
         os.makedirs(self.__folder, exist_ok=True)
 
@@ -23,13 +24,13 @@ class DataManager:
             "obstacle_data": self.obstacle_data,
             "hit_obstacles": self.hit_obstacles,
             "player_satisfaction": self.player_satisfaction,
-            "player_emotions": self.player_emotions,
+            "player_emotions": list(self.player_emotions),
         }
 
-        # with open(file_path, 'w') as f:
-        #     json.dump(data, f)
+        with open(file_path, 'w') as f:
+            json.dump(data, f)
 
-        # print(f"saved  data {file_path}")
+        print(f"saved  data {file_path}")
 
     def add_emotion(self, dominant_emotion, second_dominant_emotion):
         self.player_emotions.append((dominant_emotion, second_dominant_emotion))
