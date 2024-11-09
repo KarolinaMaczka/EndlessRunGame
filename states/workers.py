af8618f4cd872b6840273ca3d64c3d580d522cb6
Łukasz Lepianka <76851989+luki308@users.noreply.github.com>
2024-11-09 17:03:56 +0100

    fixed the start of the workers refactor: update camera reading to use multiprocessing and improve emotion analysis handling
    (cherry picked from commit aa21ad3cc9215a05e60a9d91555e9f2088100f57)

--- states/workers.py
+++ states/workers.py
@@ -5,22 +5,20 @@ from difficulty.difficulty.impl.difficulty_level2 import Difficulty2
 from difficulty.difficulty_manager import DifficultyManager
 
 
-def obstacle_generator_worker(obstacle_queue, player_z, go, difficulty_level, mapp_data_queue, **kwargs):
-    print("start generating")
+def obstacle_generator_worker(obstacle_queue, player_z, go, difficulty_level, **kwargs):
+    print('Obstacle generator worker started')
     prev = difficulty_level.value
     difficulty_level_obj = Difficulty1()
     difficulty_manager = DifficultyManager()
+    print(f'go: {go.value}')
     while go.value:
-        obstacles, map_data = difficulty_level_obj.generate_obstacle(player_z.value)
+        obstacles = difficulty_level_obj.generate_obstacle(player_z.value)
         if obstacles:
             # print(f"addded obs {obstacles[0].position_z}, {obstacles[len(obstacles) - 1].position_z}")
             if prev != difficulty_level.value:
                 difficulty_level_obj = difficulty_manager.change_level_class(difficulty_level.value, difficulty_level_obj)
                 prev=difficulty_level.value
 
-            if map_data[0]:
-                mapp_data_queue.put(map_data)
             for obstacle in obstacles:
-                print("put obstacle to queue")
                 obstacle_queue.put(obstacle)
                 time.sleep(0.1)
