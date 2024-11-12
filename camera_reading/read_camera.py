fca5db6729f6fa44b92ef47de756064875227735
KarolinaMaczka <kari.maczka@gmail.com>
2024-11-12 16:45:04 +0100

    waiting for camera

--- camera_reading/read_camera.py
+++ camera_reading/read_camera.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 import time
 
-from multiprocessing import Queue
+from multiprocessing import Queue, Event
 
 from config.logger import get_game_logger
 from data_manager import DataManager
@@ -19,9 +19,8 @@ class CameraReader:
         self.emotion_holder = EmotionHolder()
         self.game_is_running = False
         self.data_manager = data_manager
-        self.debug = False
 
-    def run(self, queue: Queue):
+    def run(self, queue: Queue, camera_ready_event: Event):
         from deepface.DeepFace import analyze # Importuję tutaj, żeby nie było problemów z importem w innych plikach (konkretnie blokuje wtedy workerów)
 
         cap = cv.VideoCapture(0)
@@ -29,20 +28,25 @@ class CameraReader:
         while True:
             ret, frame = cap.read()
             # cv.imshow('Kamera', frame)
+            if cap.isOpened():
+                camera_ready_event.set()
+            else:
+                logger.error('Failed to connect to the camera')
+                return
             if not queue.empty():
+                logger.info(f'Processing queue for camera')
                 self.game_is_running = queue.get()
             if time.time() - self.last_analysis_time > self.analysis_interval and self.game_is_running:
                 try:
                     logger.info(f'Analyzing emotions...')
                     result = analyze(frame, actions=['emotion'], enforce_detection=False, detector_backend='mtcnn')[0]
+                    logger.info(f'Result of analyzing emotions {result}')
                     self.emotion_holder.dominant_emotion = max(result['emotion'], key=result['emotion'].get)
                     self.emotion_holder.second_dominant_emotion = sorted(result['emotion'], key=result['emotion'].get)[-2]
-
+                    logger.info(
+                        f'Prevailing emotions: {self.emotion_holder.dominant_emotion} i {self.emotion_holder.second_dominant_emotion}')
                     self.data_manager.add_emotion(self.emotion_holder.dominant_emotion, self.emotion_holder.second_dominant_emotion)
-                    if self.debug:
-                        logger.info(f'Analyzing emotions...')
-                        logger.info(f'Result of analyzing emotions {result}')
-                        logger.info(f'Prevailing emotions: {self.emotion_holder.dominant_emotion} i {self.emotion_holder.second_dominant_emotion}')
+                    logger.info(f'Added emotions to data manager')
                 except Exception as e:
                     logger.error(f'Błąd podczas analizy: {e}')
                 self.last_analysis_time = time.time()
